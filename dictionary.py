1e8068f36b7e7d96980e02dd1a6111397a4d8418
Leandro Molinari <leandro.e.molinari@gds.ey.com>
2021-05-17 22:36:08 -0300

    adding dictionary file

---
+++ dictionary.py
@@ -0,0 +1,31 @@
+# Dictionary (data structure)
+# dict 
+
+dictionary = {
+    'a' : 1,
+    'b' : 2,
+    'c' : 3
+ }
+
+print(dictionary['b'])
+
+new_dic = {
+    'a' : [1,2,3,4,5],
+    'b' : True,
+    'c' : [1,[2,3,4],5]
+}
+
+print(new_dic['c'][1])
+
+
+# when use a list or when use a dictionary
+# List is order.
+# dictionary is not order.
+    #dictionary can have diferents data types like a key but is not possible set a list like a key of dictionary.
+
+# dictionary methods
+# another way for define a dictionary and is using "dict"
+
+new_dic2 = dict(name ='leandro')
+
+print(new_dic2)
