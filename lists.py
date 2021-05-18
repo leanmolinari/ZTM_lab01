20dbef52e9d60cb65dd956d2eeb3cfa65cb2c189
Leandro Molinari <leandro.e.molinari@gds.ey.com>
2021-05-17 22:09:42 -0300

    adding more lists examples

--- lists.py
+++ lists.py
@@ -5,4 +5,29 @@ li2 = ['a', 'b', 'c']
 li3 = [1,2, 3, 'Lean', True]
 
 print(li3[3])
+print(li3.count('Lean'))
 
+li.sort(reverse=True)
+li2.reverse()
+
+print(li)
+print(li2)
+
+new_big_list = list(range(1,100))
+
+print(new_big_list)
+
+# list unpacking
+
+a,b,c, *other = [1,2,3,4,5,6,7,8,9,0]
+
+print(a)
+print(b)
+print(c)
+print(other)
+
+# None
+
+game = None
+
+print(game)
