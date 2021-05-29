de47743c2df90fa3f398912ead04fb85d8afd5d9
Leandro Molinari <leandro.e.molinari@gds.ey.com>
2021-05-29 14:33:33 -0300

    add excercise file

--- loops.py
+++ loops.py
@@ -17,3 +17,19 @@ for k, v in user.items():
 for numbers in 50:
     print(numbers)
 
+# while loop
+
+
+numbers = 0
+
+while numbers < 50:
+    print(numbers)
+    numbers = numbers + 1
+else:
+    print('The work is done!')
+
+
+while True:
+    response = input('say something: ')
+    if (response == 'bye'):
+        break
