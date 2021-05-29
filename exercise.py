c7e3dbb6540d31f796b40b9c1a4cb33436f120e1
Leandro Molinari <leandro.e.molinari@gds.ey.com>
2021-05-29 15:12:16 -0300

    adding another fix to excerise file

--- exercise.py
+++ exercise.py
@@ -9,11 +9,11 @@ picture = [
     [0,0,0,1,0,0,0]
 ]
 
-
-for row in picture:
-    for pixel in row:
-        if (pixel == 1):
-            print('*', end= '')
-        else:
-            print(' ', end = '')
-    print('')
+for laps in range(2):
+    for row in picture:
+        for pixel in row:
+            if (pixel == 1):
+                print('*', end= '')
+            else:
+                print(' ', end = '')
+        print('')
