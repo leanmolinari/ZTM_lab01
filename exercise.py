c32534184bb23eb158c023eafcacb9a3bae24d4e
Leandro Molinari <leandro.e.molinari@gds.ey.com>
2021-05-30 13:09:22 -0300

    adding new fixes

--- exercise.py
+++ exercise.py
@@ -9,7 +9,19 @@ picture = [
     [0,0,0,1,0,0,0]
 ]
 
-for laps in range(2):
+#for laps in range(2):
+#    for row in picture:
+#        for pixel in row:
+#            if (pixel == 1):
+#                print('*', end= '')
+#            else:
+#               print(' ', end = '')
+#        print('')
+
+
+
+# for repease in an easy way, I'm going to put the above code in a funciton
+def show_tree():
     for row in picture:
         for pixel in row:
             if (pixel == 1):
@@ -17,3 +29,7 @@ for laps in range(2):
             else:
                 print(' ', end = '')
         print('')
+
+show_tree()
+show_tree()
+show_tree()
